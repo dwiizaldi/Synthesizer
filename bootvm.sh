a7b0c6b1e50f3add4228a13ebc6656d22e047c3d
tsangmingjiang <itriA60012@itri.org.tw>
2018-07-06 09:23:19 +0800

    a

--- bootvm.sh
+++ bootvm.sh
@@ -1,7 +1,5 @@
 #!bin/bash
 
-timecount = 300
-
 # start VMs
 echo ""
 echo -e "BOOT UP VMs"
@@ -15,7 +13,7 @@ echo -e "\n   checking VM state :\n"
 _vms=( ${vms[@]} )
 vmss=()
 n=0
-for (( i=0; i<timecount; i++ )); do
+for (( i=0; i<300; i++ )); do
 	echo -n "."
 	for vm in ${_vms[@]}; do
 		ping -c 1 -W 1 ${vm} >/dev/null 2>&1
